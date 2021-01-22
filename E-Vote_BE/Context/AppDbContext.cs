b0af3939c6c9f8622ef8a2a15adb36fdea87a934
wilmilcard <leonjuandavid@hotmail.com>
2021-01-21 19:51:54 -0500

    Agregar archivos de proyecto.

---
+++ E-Vote_BE/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Vote_BE.Context
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}
